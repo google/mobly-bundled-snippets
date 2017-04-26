26597284710a1e5bed58d6090dc23068fe4e410d
Ang Li <angli@google.com>
2017-04-26 14:55:19 -0700

    Add unit.

--- src/main/java/com/google/android/mobly/snippet/bundled/WifiManagerSnippet.java
+++ src/main/java/com/google/android/mobly/snippet/bundled/WifiManagerSnippet.java
@@ -302,7 +302,7 @@ public class WifiManagerSnippet implements Snippet {
         }
         if (!Utils.waitUntil(() -> wifiIsApEnabled() == true, 60)) {
             throw new WifiManagerSnippetException(
-                    "Timed out after 60 waiting for Wi-Fi Soft AP state to turn on with configuration: "
+                    "Timed out after 60s waiting for Wi-Fi Soft AP state to turn on with configuration: "
                             + configuration);
         }
     }
