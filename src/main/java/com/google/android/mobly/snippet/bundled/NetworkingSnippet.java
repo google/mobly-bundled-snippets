5ac0238781be7b3de7c91b46888d0d81374798e1
Keith Dart <dart@google.com>
2017-09-22 21:39:26 -0700

    Add a method to perform an HTTP download using DownloadManager.
    Still needs a little work getting the local location reported back.

--- src/main/java/com/google/android/mobly/snippet/bundled/NetworkingSnippet.java
+++ src/main/java/com/google/android/mobly/snippet/bundled/NetworkingSnippet.java
@@ -20,12 +20,30 @@ import java.net.InetAddress;
 import java.net.Socket;
 import java.io.IOException;
 import java.net.UnknownHostException;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.BroadcastReceiver;
+import android.net.Uri;
+import android.os.Environment;
+import android.app.DownloadManager;
+import android.support.test.InstrumentationRegistry;
 import com.google.android.mobly.snippet.Snippet;
+import com.google.android.mobly.snippet.bundled.utils.Utils;
 import com.google.android.mobly.snippet.rpc.Rpc;
 import com.google.android.mobly.snippet.util.Log;
 
 /** Snippet class for networking RPCs. */
 public class NetworkingSnippet implements Snippet {
+
+    private final DownloadManager mDownloadManager;
+    private final Context mContext;
+    private volatile boolean mIsDownloadComplete = false;
+
+    public NetworkingSnippet() {
+        mContext = InstrumentationRegistry.getContext();
+        mDownloadManager = (DownloadManager) mContext.getSystemService(Context.DOWNLOAD_SERVICE);
+    }
 
     @Rpc(description = "Check if a host and port are connectable using a TCP connection attempt.")
     public boolean networkIsTcpConnectable(String host, int port) {
@@ -45,6 +63,46 @@ public class NetworkingSnippet implements Snippet {
             return false;
         }
         return true;
+    }
+
+    @Rpc(description = "Download a file using HTTP.")
+    public String networkHTTPDownload(String url, String destination) {
+        long reqid = 0;
+        Uri uri = Uri.parse(url);
+        DownloadManager.Request request = new DownloadManager.Request(uri);
+        request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, destination);
+        mIsDownloadComplete = false;
+        IntentFilter filter = new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE);
+        BroadcastReceiver receiver = new DownloadReceiver();
+        mContext.registerReceiver(receiver, filter);
+        try {
+            reqid = mDownloadManager.enqueue(request);
+            Log.d(String.format("networkHTTPDownload download of %s with id %d", url, reqid));
+            if (!Utils.waitUntil(() -> mIsDownloadComplete, 240)) {
+                return "";
+            }
+        } finally {
+            mContext.unregisterReceiver(receiver);
+        }
+
+        Uri resp = mDownloadManager.getUriForDownloadedFile(reqid);
+        if (resp != null) {
+            Log.d(String.format("networkHTTPDownload completed to %s", resp.toString()));
+            return resp.toString();
+        } else {
+            return "";
+        }
+    }
+
+    private class DownloadReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (DownloadManager.ACTION_DOWNLOAD_COMPLETE.equals(action)) {
+                mIsDownloadComplete = true;
+            }
+        }
     }
 
     @Override
