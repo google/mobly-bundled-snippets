689db412435f15945fd866a28a0f63d70495276c
haoma <haoma@google.com>
2024-01-04 15:19:21 +0800

    Add new function: String getPhoneNumber(int simSlot) Add new variant: int getTelephonyCallState(int simSlot)

--- src/main/java/com/google/android/mobly/snippet/bundled/TelephonySnippet.java
+++ src/main/java/com/google/android/mobly/snippet/bundled/TelephonySnippet.java
@@ -18,6 +18,8 @@ package com.google.android.mobly.snippet.bundled;
 
 import android.content.Context;
 import android.os.Build;
+import android.telephony.SubscriptionInfo;
+import android.telephony.SubscriptionManager;
 import android.telephony.TelephonyManager;
 import androidx.test.platform.app.InstrumentationRegistry;
 import com.google.android.mobly.snippet.Snippet;
@@ -27,15 +29,33 @@ import com.google.android.mobly.snippet.rpc.Rpc;
 public class TelephonySnippet implements Snippet {
 
     private final TelephonyManager mTelephonyManager;
+    private final SubscriptionManager mSubscriptionManager;
 
     public TelephonySnippet() {
         Context context = InstrumentationRegistry.getInstrumentation().getContext();
         mTelephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+        mSubscriptionManager = (SubscriptionManager) context.getSystemService(Context.TELEPHONY_SUBSCRIPTION_SERVICE);
     }
 
     @Rpc(description = "Gets the line 1 phone number.")
     public String getLine1Number() {
         return mTelephonyManager.getLine1Number();
+    }
+
+    @Rpc(description = "Gets phone number for the simSlot (slot# start from 0, only valid for API level > 32)."
+                                + " API level <33 will always return phone number for simSlotIndex = 0")
+    public String getPhoneNumber(int simSlot) {
+        String thisNumber = "";
+
+        if (Build.VERSION.SDK_INT < 33) {
+            thisNumber = mTelephonyManager.getLine1Number();
+        } else {
+            SubscriptionInfo mSubscriptionInfo = mSubscriptionManager.getActiveSubscriptionInfoForSimSlotIndex(simSlot);
+            if (mSubscriptionInfo != null) {
+                thisNumber = mSubscriptionManager.getPhoneNumber(mSubscriptionInfo.getSubscriptionId());
+            }
+        }
+        return thisNumber;
     }
 
     @Rpc(description = "Returns the unique subscriber ID, for example, the IMSI for a GSM phone.")
@@ -53,6 +73,28 @@ public class TelephonySnippet implements Snippet {
         } else {
             return mTelephonyManager.getCallStateForSubscription();
         }
+    }
+
+    @Rpc(
+            description =
+                    "Gets the call state for the simSlot (slot# start from 0, only valid for API level > 30). Call state values are"
+                            + "0: IDLE, 1: RINGING, 2: OFFHOOK.")
+    public int getTelephonyCallState(int simSlot) {
+        int thisState = -1;
+
+        if (Build.VERSION.SDK_INT < 31) {
+            thisState = mTelephonyManager.getCallState();
+        } else {
+            SubscriptionInfo mSubscriptionInfo = mSubscriptionManager.getActiveSubscriptionInfoForSimSlotIndex(simSlot);
+            if (mSubscriptionInfo != null) {
+                thisState =
+                        mTelephonyManager
+                                .createForSubscriptionId(mSubscriptionInfo.getSubscriptionId())
+                                .getCallStateForSubscription();
+            }
+        }
+
+        return thisState;
     }
 
     @Rpc(
