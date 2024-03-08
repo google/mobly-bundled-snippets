142640c4118db0043809e4885989c63b4df4d534
Nihal <nihald2000@gmail.com>
2024-03-08 18:46:26 +0530

    Refactor Bluetooth device management
    In this commit, the connect and disconnect APIs have been removed, and the BluetoothHeadsetClient file has been eliminated.
    Instead, a new BluetoothHeadset file has been added.
    BluetoothHeadsetClient device is primarily focused on receiving audio for voice calls.Example:car-infotainment
    The changes align with best practices for Bluetooth device management.
    The project has been successfully rebuilt after these changes.

--- src/main/java/com/google/android/mobly/snippet/bundled/bluetooth/profiles/BluetoothHeadsetSnippet.java
+++ src/main/java/com/google/android/mobly/snippet/bundled/bluetooth/profiles/BluetoothHeadsetSnippet.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 Google Inc.
+ * Copyright (C) 2024 Google Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -14,15 +14,12 @@
  * the License.
  */
 
-package com.google.android.mobly.snippet.bundled.bluetooth;
+package com.google.android.mobly.snippet.bundled.bluetooth.profiles;
 
-import android.bluetooth.BluetoothA2dp;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothProfile;
-import android.bluetooth.BluetoothA2dpSink;
-import android.bluetooth.BluetoothHeadsetClient;
-import android.bluetooth.BluetoothHeadsetClientCall;
+import android.bluetooth.BluetoothHeadset;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -42,28 +39,23 @@ import java.util.Set;
 import java.util.List;
 import android.util.Log;
 
-public class BluetoothHeadsetClientSnippet implements Snippet {
-    private static class BluetoothHeadsetClientSnippetException extends Exception {
+public class BluetoothHeadsetSnippet implements Snippet {
+    private static class BluetoothHeadsetSnippetException extends Exception {
         private static final long serialVersionUID = 1;
 
-        BluetoothHeadsetClientSnippetException(String msg) {
+        BluetoothHeadsetSnippetException(String msg) {
             super(msg);
         }
     }
 
-    private final String TAG = "BluetoothHeadsetClientSnippet";
+    private final String TAG = "BluetoothHeadsetSnippet";
     private static boolean sIsHFPProfileReady = false;
     private Context mContext;
-    private BluetoothHeadsetClient mBluetoothHeadsetClient;
+    private BluetoothHeadset mBluetoothHeadset;
     private BluetoothProfile.ServiceListener mServiceListenner;
-    private static final int HEADSET_CLIENT = 16;
+    private static final int HEADSET = 1;
     private final JsonSerializer mJsonSerializer = new JsonSerializer();
     private static final BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-    private BluetoothHeadsetClientCall m_CurrentCall = null;
-    private boolean isAudioStateChanged = false;
-    private boolean isCallStateChanged = false;
-    private int bluetoothHeadsetAudioState = BluetoothHeadsetClient.STATE_AUDIO_DISCONNECTED;
-    private int m_CurrentCallState = BluetoothHeadsetClientCall.CALL_STATE_TERMINATED;
     private BluetoothDevice mBluetoothConnectedDevice;
     private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
         @Override
@@ -71,12 +63,7 @@ public class BluetoothHeadsetClientSnippet implements Snippet {
             Log.d(TAG, "BroadcastReceiver onReceive");
             String action = intent.getAction();
             android.util.Log.d(TAG, action);
-            if (BluetoothHeadsetClient.ACTION_CALL_CHANGED.equals(action)) {
-                android.util.Log.d(TAG, action);
-                m_CurrentCall = intent.getParcelableExtra(BluetoothHeadsetClient.EXTRA_CALL);
-                android.util.Log.d(TAG, m_CurrentCall.toString());
-            }
-            if (BluetoothHeadsetClient.ACTION_CONNECTION_STATE_CHANGED.equals(action)) {
+            if (BluetoothHeadset.ACTION_CONNECTION_STATE_CHANGED.equals(action)) {
                 android.util.Log.d(TAG, action);
             }
         }
@@ -84,78 +71,31 @@ public class BluetoothHeadsetClientSnippet implements Snippet {
     private final BluetoothProfile.ServiceListener mProfileListener = new BluetoothProfile.ServiceListener() {
         public void onServiceConnected(int var1, BluetoothProfile profile) {
             Log.d(TAG, "onServiceConnected" + var1);
-            if (var1 == HEADSET_CLIENT) {
-                mBluetoothHeadsetClient = (BluetoothHeadsetClient) profile;
+            if (var1 == HEADSET) {
+                mBluetoothHeadset = (BluetoothHeadset)profile;
                 sIsHFPProfileReady = true;
             }
         }
 
         public void onServiceDisconnected(int var1) {
             Log.d(TAG, "onServiceDisconnected" + var1);
-            if (var1 == HEADSET_CLIENT) {
+            if (var1 == HEADSET) {
                 sIsHFPProfileReady = false;
-                mBluetoothHeadsetClient = null;
+                mBluetoothHeadset = null;
             }
         }
     };
 
-    public BluetoothHeadsetClientSnippet() {
-        Log.d(TAG, "BluetoothHeadsetClientSnippet");
+    public BluetoothHeadsetSnippet() {
+        Log.d(TAG, "BluetoothHeadsetSnippet");
         mContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        IntentFilter filter = new IntentFilter(BluetoothHeadsetClient.ACTION_CALL_CHANGED);
-        filter.addAction(BluetoothHeadsetClient.ACTION_CONNECTION_STATE_CHANGED);
-        filter.addAction(BluetoothHeadsetClient.ACTION_AUDIO_STATE_CHANGED);
-        mBluetoothAdapter.getProfileProxy(mContext, mProfileListener, HEADSET_CLIENT);
+        IntentFilter filter = new IntentFilter(BluetoothHeadset.ACTION_CONNECTION_STATE_CHANGED);
+        filter.addAction(BluetoothHeadset.ACTION_AUDIO_STATE_CHANGED);
+        mBluetoothAdapter.getProfileProxy(mContext, mProfileListener, HEADSET);
         Utils.waitUntil(() -> sIsHFPProfileReady, 60);
         mContext.registerReceiver(mReceiver, filter);
     }
 
-    @Rpc(description = "Set connection policy for HFP profile.")
-    public boolean btheadsetClientset(String name, int connectionPolicy) {
-        Log.d(TAG, "btheadsetClientset");
-        if (mBluetoothHeadsetClient == null) {
-            android.util.Log.d(TAG, "btheadsetClientset mBluetoothHeadsetClient is NULL");
-            return false;
-        } else {
-            Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
-            for (BluetoothDevice device : pairedDevices) {
-                if (device.getName().equalsIgnoreCase(name)) {
-                    boolean status = mBluetoothHeadsetClient.setConnectionPolicy(device, connectionPolicy);
-                    Log.d(TAG, "btheadsetClientset status:" + status);
-                    return status;
-                }
-            }
-        }
-        return false;
-    }
-
-    @Rpc(description = "Connects to remote device.")
-    public boolean btHfpconnect(String name) {
-        Log.d(TAG, "btHfpcconnect");
-        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
-        for (BluetoothDevice device : pairedDevices) {
-            if (device.getName().equalsIgnoreCase(name)) {
-                boolean status = mBluetoothHeadsetClient.connect(device);
-                Log.d(TAG, "btHfpcconnect status:" + status);
-                return status;
-            }
-        }
-        return false;
-    }
-
-    @Rpc(description = "disconnects to remote device.")
-    public boolean btHfpdisconnect(String name) {
-        Log.d(TAG, "btHfpcdisconnect");
-        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
-        for (BluetoothDevice device : pairedDevices) {
-            if (device.getName().equalsIgnoreCase(name)) {
-                boolean status = mBluetoothHeadsetClient.disconnect(device);
-                Log.d(TAG, "btHfpcdisconnect status:" + status);
-                return status;
-            }
-        }
-        return false;
-    }
 
     @Rpc(description = " Returns connection state.")
     public int btHfpgetConnectionState(String name) {
@@ -164,7 +104,7 @@ public class BluetoothHeadsetClientSnippet implements Snippet {
         for (BluetoothDevice device : pairedDevices) {
             if (device.getName().equalsIgnoreCase(name)) {
                 Log.d(TAG, "btHfpgetConnectionState");
-                return mBluetoothHeadsetClient.getConnectionState(device);
+                return mBluetoothHeadset.getConnectionState(device);
             }
         }
         return 0;
@@ -175,7 +115,7 @@ public class BluetoothHeadsetClientSnippet implements Snippet {
         Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
         for (BluetoothDevice device : pairedDevices) {
             if (device.getName().equalsIgnoreCase(name)) {
-                return mBluetoothHeadsetClient.startVoiceRecognition(device);
+                return mBluetoothHeadset.startVoiceRecognition(device);
             }
         }
         return false;
@@ -186,7 +126,7 @@ public class BluetoothHeadsetClientSnippet implements Snippet {
         Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
         for (BluetoothDevice device : pairedDevices) {
             if (device.getName().equalsIgnoreCase(name)) {
-                return mBluetoothHeadsetClient.stopVoiceRecognition(device);
+                return mBluetoothHeadset.stopVoiceRecognition(device);
             }
         }
         return false;
