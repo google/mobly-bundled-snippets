206c5898f6edc8ebb3eaaf387413afa0da5d1e21
Kolin Lu <kolinlu@google.com>
2025-02-19 16:27:26 -0800

    Support connect/disconnect HFP and LE Audio

--- src/main/java/com/google/android/mobly/snippet/bundled/bluetooth/profiles/BluetoothLeAudioSnippet.java
+++ src/main/java/com/google/android/mobly/snippet/bundled/bluetooth/profiles/BluetoothLeAudioSnippet.java
@@ -18,13 +18,17 @@ package com.google.android.mobly.snippet.bundled.bluetooth.profiles;
 
 import android.annotation.TargetApi;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothLeAudio;
 import android.bluetooth.BluetoothProfile;
 import android.content.Context;
+import android.content.IntentFilter;
 import android.os.Build;
 import android.os.Bundle;
 import androidx.test.platform.app.InstrumentationRegistry;
 import com.google.android.mobly.snippet.Snippet;
+import com.google.android.mobly.snippet.bundled.bluetooth.BluetoothAdapterSnippet;
+import com.google.android.mobly.snippet.bundled.bluetooth.PairingBroadcastReceiver;
 import com.google.android.mobly.snippet.bundled.utils.JsonSerializer;
 import com.google.android.mobly.snippet.bundled.utils.Utils;
 import com.google.android.mobly.snippet.rpc.Rpc;
@@ -33,17 +37,71 @@ import java.util.ArrayList;
 
 /** Snippet class exposing Bluetooth LE Audio profile. */
 public class BluetoothLeAudioSnippet implements Snippet {
+    private static class BluetoothLeAudioSnippetException extends Exception {
+        private static final long serialVersionUID = 1;
 
+        /**
+         * Constructs a BluetoothLeAudioSnippetException with the specified detail message.
+         *
+         * @param msg The detail message providing information about the exception.
+         */
+        BluetoothLeAudioSnippetException(String msg) {
+            super(msg);
+        }
+    }
+
+    private final Context mContext;
     private static boolean sIsLeAudioProfileReady = false;
     private static BluetoothLeAudio sLeAudioProfile;
     private final JsonSerializer mJsonSerializer = new JsonSerializer();
 
     public BluetoothLeAudioSnippet() {
-        Context mContext = InstrumentationRegistry.getInstrumentation().getContext();
+        mContext = InstrumentationRegistry.getInstrumentation().getContext();
         BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         bluetoothAdapter.getProfileProxy(
                 mContext, new LeAudioServiceListener(), BluetoothProfile.LE_AUDIO);
         Utils.waitUntil(() -> sIsLeAudioProfileReady, 60);
+    }
+
+    @TargetApi(Build.VERSION_CODES.TIRAMISU)
+    @RpcMinSdk(Build.VERSION_CODES.TIRAMISU)
+    @Rpc(
+        description =
+            "Connects to a paired or discovered device with LE Audio profile."
+                + "If a device has been discovered but not paired, this will pair it.")
+    public void btLeAudioConnect(String deviceAddress) throws Throwable {
+        BluetoothDevice device = BluetoothAdapterSnippet.getKnownDeviceByAddress(deviceAddress);
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_PAIRING_REQUEST);
+        mContext.registerReceiver(new PairingBroadcastReceiver(mContext), filter);
+        Utils.invokeByReflection(sLeAudioProfile, "connect", device);
+        if (!Utils.waitUntil(
+            () -> sLeAudioProfile.getConnectionState(device) == BluetoothProfile.STATE_CONNECTED,
+            120)) {
+            throw new BluetoothLeAudioSnippetException(
+                "Failed to connect to device "
+                    + device.getName()
+                    + "|"
+                    + device.getAddress()
+                    + " with LE Audio profile within 2min.");
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.TIRAMISU)
+    @RpcMinSdk(Build.VERSION_CODES.TIRAMISU)
+    @Rpc(description = "Disconnects a device from LE Audio profile.")
+    public void btLeAudioDisconnect(String deviceAddress) throws Throwable {
+        BluetoothDevice device = getConnectedBluetoothDevice(deviceAddress);
+        Utils.invokeByReflection(sLeAudioProfile, "disconnect", device);
+        if (!Utils.waitUntil(
+            () -> sLeAudioProfile.getConnectionState(device) == BluetoothProfile.STATE_DISCONNECTED,
+            120)) {
+            throw new BluetoothLeAudioSnippetException(
+                "Failed to disconnect device "
+                    + device.getName()
+                    + "|"
+                    + device.getAddress()
+                    + " from LE Audio profile within 2min.");
+        }
     }
 
     /** Service Listener for {@link BluetoothLeAudio}. */
@@ -66,5 +124,16 @@ public class BluetoothLeAudioSnippet implements Snippet {
     @Rpc(description = "Gets all the devices currently connected via LE Audio profile.")
     public ArrayList<Bundle> btLeAudioGetConnectedDevices() {
         return mJsonSerializer.serializeBluetoothDeviceList(sLeAudioProfile.getConnectedDevices());
+    }
+
+    private BluetoothDevice getConnectedBluetoothDevice(String deviceAddress)
+        throws BluetoothLeAudioSnippetException {
+        for (BluetoothDevice device : sLeAudioProfile.getConnectedDevices()) {
+            if (device.getAddress().equalsIgnoreCase(deviceAddress)) {
+                return device;
+            }
+        }
+        throw new BluetoothLeAudioSnippetException(
+            "No device with address " + deviceAddress + " is connected via LE Audio.");
     }
 }
