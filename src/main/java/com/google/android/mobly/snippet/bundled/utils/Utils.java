d3d38b5faee50fbdd90dc5ae851a08214be07857
ybh2026 <yabinh@google.com>
2026-06-10 02:38:48 -0700

    Avoid destroying UiAutomation to keep adopted permissions (#254)

--- src/main/java/com/google/android/mobly/snippet/bundled/utils/Utils.java
+++ src/main/java/com/google/android/mobly/snippet/bundled/utils/Utils.java
@@ -236,18 +236,20 @@ public final class Utils {
                         + Arrays.toString(permissions));
                 uia.adoptShellPermissionIdentity(permissions);
             }
-            // Need to drop the UI Automation to allow other snippets to get access
-            // to global UI automation.
-            // Using reflection here since the method is not public.
-            try {
-                Class<?> cls = Class.forName("android.app.UiAutomation");
-                Method destroyMethod = cls.getDeclaredMethod("destroy");
-                destroyMethod.invoke(uia);
-            } catch (NoSuchMethodException
-                    | IllegalAccessException
-                    | ClassNotFoundException
-                    | InvocationTargetException e) {
-                throw new RuntimeException("Failed to cleaup Ui Automation", e);
+            if (Build.VERSION.SDK_INT < 37) {
+                // Need to drop the UI Automation to allow other snippets to get access
+                // to global UI automation.
+                // Using reflection here since the method is not public.
+                try {
+                    Class<?> cls = Class.forName("android.app.UiAutomation");
+                    Method destroyMethod = cls.getDeclaredMethod("destroy");
+                    destroyMethod.invoke(uia);
+                } catch (NoSuchMethodException
+                        | IllegalAccessException
+                        | ClassNotFoundException
+                        | InvocationTargetException e) {
+                    throw new RuntimeException("Failed to cleanup Ui Automation", e);
+                }
             }
         }
     }
